5f2634b14747679f5e380f6e3bfcd7625fa2008a
包凯瑞 <673575209@qq.com>
2020-10-31 09:33:18 +0800

    warm-up代码规范修改

--- plot_matrix.py
+++ plot_matrix.py
@@ -1,14 +1,19 @@
-# TODO: import ...
 import numpy as np
 import matplotlib.image as plt
+
+
 def generate_random_matrix(m, n):
+
     image_array = np.random.randint(0, 2, (m, n))
     return image_array
 
 
 def save_matrix(matrix, file_name):
+
     plt.imsave(file_name, matrix)
-    
+
+
 if __name__ == "__main__":
+
     matrix = generate_random_matrix(10, 10)
     save_matrix(matrix, "example.jpg")
